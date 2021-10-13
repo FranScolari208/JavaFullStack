54d5fbf239d100a7f5043f223b9827671972dcd5
Franco Scolari <franco.scolari@hotmail.com>
2021-10-12 22:15:19 -0300

    Ejercicio 11 y Guia SpringBoot

---
+++ GuiaSpringBoot/libreria.springboot/src/test/java/guiaSpring/egg/libreria/springboot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package guiaSpring.egg.libreria.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

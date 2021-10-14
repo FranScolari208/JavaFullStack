b0cfb51add58fbc857e8b2360668f8e4118607fd
Franco Scolari <franco.scolari@hotmail.com>
2021-10-14 17:28:15 -0300

    Agrego carpetas

---
+++ GuiaSpringBoot/Ejercicio2/src/main/java/ejercicio2/egg/estancia/EstanciaApplication.java
@@ -0,0 +1,13 @@
+package ejercicio2.egg.estancia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstanciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstanciaApplication.class, args);
+	}
+
+}
---
+++ GuiaSpringBoot/Ejercicio2/src/test/java/ejercicio2/egg/estancia/EstanciaApplicationTests.java
@@ -0,0 +1,13 @@
+package ejercicio2.egg.estancia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstanciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

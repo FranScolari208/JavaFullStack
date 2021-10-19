1932b7bce8d22fa90bec06a00cf4f66a9f7774b4
Franco Scolari <franco.scolari@hotmail.com>
2021-10-18 22:06:04 -0300

    Encaro libreria web

---
+++ GuiaSpringBoot/Ejercicio1/src/main/java/ejercicio1/egg/libreria/entidades/Libro.java
@@ -0,0 +1,123 @@
+package ejercicio1.egg.libreria.entidades;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Libro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long isbn;    
+    @Column(nullable = false)
+    private String titulo;
+    @Column(nullable = false)
+    private Integer anio;
+    @Column(nullable = false)
+    private Integer ejemplares;
+    @Column(nullable = false)
+    private Integer ejemplaresPrestados;
+    @Column(nullable = false)
+    private Integer ejemplaresRestantes;
+    @Column(nullable = false)
+    private Boolean alta;
+    @JoinColumn(nullable = false)
+    @ManyToOne
+    private Autor autor;
+    @JoinColumn(nullable = false)
+    @ManyToOne
+    private Editorial editorial;
+
+    public Libro() {
+    }
+
+    public Libro(Long isbn, String titulo, Integer anio, Integer ejemplares, Integer ejemplaresPrestados, Integer ejemplaresRestantes, Boolean alta, Autor autor, Editorial editorial) {
+        this.isbn = isbn;
+        this.titulo = titulo;
+        this.anio = anio;
+        this.ejemplares = ejemplares;
+        this.ejemplaresPrestados = ejemplaresPrestados;
+        this.ejemplaresRestantes = ejemplaresRestantes;
+        this.alta = alta;
+        this.autor = autor;
+        this.editorial = editorial;
+    }
+
+    public Long getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(Long isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getAnio() {
+        return anio;
+    }
+
+    public void setAnio(Integer anio) {
+        this.anio = anio;
+    }
+
+    public Integer getEjemplares() {
+        return ejemplares;
+    }
+
+    public void setEjemplares(Integer ejemplares) {
+        this.ejemplares = ejemplares;
+    }
+
+    public Integer getEjemplaresPrestados() {
+        return ejemplaresPrestados;
+    }
+
+    public void setEjemplaresPrestados(Integer ejemplaresPrestados) {
+        this.ejemplaresPrestados = ejemplaresPrestados;
+    }
+
+    public Integer getEjemplaresRestantes() {
+        return ejemplaresRestantes;
+    }
+
+    public void setEjemplaresRestantes(Integer ejemplaresRestantes) {
+        this.ejemplaresRestantes = ejemplaresRestantes;
+    }
+
+    public Boolean getAlta() {
+        return alta;
+    }
+
+    public void setAlta(Boolean alta) {
+        this.alta = alta;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+    public Editorial getEditorial() {
+        return editorial;
+    }
+
+    public void setEditorial(Editorial editorial) {
+        this.editorial = editorial;
+    }
+    
+    
+}
